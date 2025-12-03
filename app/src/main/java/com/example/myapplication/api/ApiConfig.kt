d46198b6bb40a1706ea066e5b543335c65b5cdfb
Crisapps99 <158056254+Crisapps99@users.noreply.github.com>
2025-12-02 23:22:18 -0500

    Segundo commit agregando api de neurona artifical intenciones Respuestas Permisos Reconocimiento de voz a texto envio de texto a voz

--- app/src/main/java/com/example/myapplication/api/ApiConfig.kt
+++ app/src/main/java/com/example/myapplication/api/ApiConfig.kt
@@ -1,10 +1,7 @@
 package com.example.myapplication.api
-
 import com.google.gson.annotations.SerializedName
-import org.w3c.dom.Text
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import retrofit2.http.Body
 import retrofit2.http.POST
 
